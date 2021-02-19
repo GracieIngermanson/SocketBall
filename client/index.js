25aac336a25b27112a44d6cb40f578ad7009ada9
Gracie Ingermanson <gmi@umich.edu>
2021-02-18 22:05:47 -0500

    Zoom-like text chat application

--- client/index.js
+++ client/index.js
@@ -1,19 +1,81 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {Router} from 'react-router-dom'
-import history from './history'
-import store from './store'
-import App from './app'
+import io from 'socket.io-client'
 
-// establishes socket connection
-import './socket'
+const clientSocket = io(window.location.origin)
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('app')
-)
+let username = ''
+let chatRoomName = ''
+let chatParticipants = {}
+
+const entryForm = document.getElementById('entry-form')
+const usernameInput = document.getElementById('username')
+const chatRoomNameInput = document.getElementById('chat-room-name')
+const entryButton = document.getElementById('entry-button')
+
+const chatRoom = document.getElementById('chat-room')
+const chatForm = document.getElementById('chat-form')
+const participantsSelect = document.getElementById('participants-select')
+const messageInput = document.getElementById('message')
+const chatMessages = document.getElementById('chat-messages')
+const sendButton = document.getElementById('send-button')
+
+entryButton.addEventListener('click', event => {
+  event.preventDefault()
+  username = usernameInput.value
+  chatRoomName = chatRoomNameInput.value
+
+  entryButton.disabled = true
+  entryForm.style.display = 'none'
+  chatRoom.style.display = 'flex'
+  sendButton.disabled = false
+  clientSocket.emit('enterChatRoom', username, chatRoomName)
+})
+
+const appendMessage = (sender, recipient, message) => {
+  chatMessages.innerHTML += `<p><strong>${sender} (to: &nbsp${recipient})</strong>: ${message}</p>`
+}
+
+const submitMessage = event => {
+  event.preventDefault()
+  let message = messageInput.value
+  messageInput.value = ''
+  appendMessage(
+    'Me',
+    participantsSelect.options[participantsSelect.selectedIndex].text,
+    message
+  )
+  if (participantsSelect.value === chatRoomName) {
+    clientSocket.emit('sendChat', chatRoomName, message)
+  } else {
+    clientSocket.emit('sendDM', participantsSelect.value, message)
+  }
+}
+
+// Update the dropdown menu of chat participants to send direct messages to
+const updateParticipants = participants => {
+  // Save a copy of the participants object
+  chatParticipants = participants
+  participantsSelect.innerHTML = `<option value=${chatRoomName} selected>Everyone</option>`
+  Object.keys(chatParticipants).forEach(participantId => {
+    // Show the user a list of everyone else
+    if (participantId !== clientSocket.id) {
+      participantsSelect.innerHTML += `<option value=${participantId}>${
+        participants[participantId].username
+      }</option>`
+    }
+  })
+  chatForm.addEventListener('submit', submitMessage)
+}
+
+const displayDM = (senderId, message) => {
+  appendMessage(chatParticipants[senderId].username, 'Me', message)
+}
+
+const displayChat = (senderId, message) => {
+  appendMessage(chatParticipants[senderId].username, 'Everyone', message)
+}
+
+clientSocket.on('updateParticipants', updateParticipants)
+clientSocket.on('displayDM', displayDM)
+clientSocket.on('displayChat', displayChat)
+
+export default clientSocket
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,9 +1,51 @@
-module.exports = io => {
-  io.on('connection', socket => {
-    console.log(`A socket connection to the server has been made: ${socket.id}`)
+// Create an object for storing data about each chat room
+// Each chat room name will store an object mapping socket ids
+// to information about the corresponding user
+const chatRooms = {}
 
-    socket.on('disconnect', () => {
-      console.log(`Connection ${socket.id} has left the building`)
+const enterChatRoom = (serverSocket, serverIO, username, chatRoomName) => {
+  // Join the room
+  serverSocket.join(chatRoomName)
+  if (!chatRooms[chatRoomName]) {
+    // First user to join the chat––add an empty object for keeping track of users
+    chatRooms[chatRoomName] = {}
+  }
+  const participants = chatRooms[chatRoomName]
+  // Store the new user's information to the participants object
+  participants[serverSocket.id] = {username}
+  // Let everyone in the chat know who the current participants are
+  serverIO.in(chatRoomName).emit('updateParticipants', participants)
+}
+
+// Callback invoked when serverSocket is about to disconnect
+const leaveChatRoom = (serverSocket, serverIO) => {
+  console.log(chatRooms)
+  for (let chatRoomName of Object.keys(chatRooms)) {
+    let participants = chatRooms[chatRoomName]
+    // Find any chat rooms that serverSocket belonged to
+    if (participants[serverSocket.id]) {
+      delete participants[serverSocket.id]
+      // Let the users still in the chat know who the remaining participants are
+      serverIO.in(chatRoomName).emit('updateParticipants', participants)
+    }
+  }
+}
+module.exports = serverIO => {
+  serverIO.on('connection', serverSocket => {
+    serverSocket.on('enterChatRoom', (username, chatRoomName) =>
+      enterChatRoom(serverSocket, serverIO, username, chatRoomName)
+    )
+
+    serverSocket.on('sendDM', (recipientId, message) => {
+      serverSocket.to(recipientId).emit('displayDM', serverSocket.id, message)
     })
+
+    serverSocket.on('sendChat', (chatRoomName, message) => {
+      serverSocket
+        .to(chatRoomName)
+        .emit('displayChat', serverSocket.id, message)
+    })
+
+    serverSocket.on('disconnect', () => leaveChatRoom(serverSocket, serverIO))
   })
 }
